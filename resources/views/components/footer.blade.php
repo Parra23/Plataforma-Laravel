79073a0abee3178ca492ecd88a5f12ea52b1740e
jparr <165439239+Parra23@users.noreply.github.com>
2025-01-07 22:44:22 -0500

    Update footer.blade.php

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="footer_part1">
     <div class="container-fluid">
-        <div class="row">
+        <div class="row footer_part2">
             <div class="col-4">
                 <h3>CONTACTO</h3>
                 <p>Telefono: 123456789</p>
