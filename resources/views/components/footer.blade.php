5a98e5a3c3ac2d1ec72a7212f5ff4aad6ad3eee6
jparr <165439239+Parra23@users.noreply.github.com>
2025-01-07 22:43:00 -0500

    Footer example

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,12 +1,16 @@
 <footer class="footer_part1">
-    <div class="container">
+    <div class="container-fluid">
         <div class="row">
-
+            <div class="col-4">
+                <h3>CONTACTO</h3>
+                <p>Telefono: 123456789</p>
+                <p>Email:
+            </div>
         </div>
         <div class="row footer_part3">
-        <div class="col-4">HOLAAAAA </div>
-        <div class="col-4">HELLOOOOO </div>
-        <div class="col-4"> MAAAAS</div>
+            <div class="col-4">HOLAAAAA </div>
+            <div class="col-4">HELLOOOOO </div>
+            <div class="col-4"> MAAAAS</div>
         </div>
     </div>
     <!-- Footer final -->
